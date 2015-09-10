d4f9a296ae66c873e7893d011f6fb2a071228767
d3z41k <d3z41k@gmail.com>
2015-09-10 16:45:07 +0300

    stage 5

--- backend/views/site/wsserver.php
+++
@@ -1,8 +0,0 @@
-<?php
-use yii\helpers\Html;
-
-echo '<b>WebSocket Server</b>';
-
-?>
-<br>
-<?= Html::a('Start Server', ['wsserver'], ['class' => 'btn btn-success', 'name' => 'On', 'value' => '1']) ?>
